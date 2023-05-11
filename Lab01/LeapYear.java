63a200671496614ce2382affc43637c8ce10a0ed
EdwinXu108 <133124291+EdwinXu108@users.noreply.github.com>
2023-05-11 17:15:43 +0800

    Update LeapYear.java

--- Lab01/LeapYear.java
+++ Lab01/LeapYear.java
@@ -1,19 +1,20 @@
 public class LeapYear {
     public static boolean isLeapYear(int year) {
-        if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0) {
-            return true;
-        }
-        return false;
+        return (year % 400 == 0 ) || (year % 4 == 0 && year % 100 != 0);
     }
-
-    public static void main(String[] args) {
-        int year = Integer.parseInt(args[0]);
+    
+    public static void checkLeapYear (int year) {
         if (isLeapYear(year)) {
             System.out.println(year + " is a leap year.");
         }
         else {
             System.out.println(year + " is not a leap year.");
         }
-        
     }
+
+    public static void main(String[] args) {
+        int year = Integer.parseInt(args[0]);
+        checkLeapYear(year);
+    }
+        
 }
